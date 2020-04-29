c6c135df98b03ee95bff163ba23bc88c6cbdf350
Jhon Harry Valdera Contreras <vcontrerasjhon@gmail.com>
2020-04-28 21:44:16 -0500

    Suma de Numeros

--- ProyectoVB_PruebaGitHub/ProyectoVB_PruebaGitHub/Module1.vb
+++ ProyectoVB_PruebaGitHub/ProyectoVB_PruebaGitHub/Module1.vb
@@ -2,6 +2,13 @@
 
     Sub Main()
         Console.WriteLine("Esto es una prueba en Git Hub")
+        Dim numero1 As Integer
+        Dim numero2 As Integer
+        Dim suma As Integer
+        numero1 = 3
+        numero2 = 5
+        suma = numero1 + numero2
+        Console.WriteLine(suma)
         Console.Read()
     End Sub
 
